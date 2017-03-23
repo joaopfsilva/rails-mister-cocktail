a0300a1db77d33dd3b478a78a6619e6384db3e00
joao silva <j0a0.silva.kle@gmail.com>
2017-03-23 12:12:19 +0100

    added controller and model

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,31 @@
+class CocktailsController < ApplicationController
+
+  before_action :do_more_magic, only: [:show, :edit, :destroy]
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def index
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def do_more_magic
+    @cocktail = Cocktail.find(params[:id])
+  end
+end
